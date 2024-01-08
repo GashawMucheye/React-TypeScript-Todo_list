745823441422962e7b999a8fd37bfe2858f9d512
gashaw mucheye <gashaw2533@gmail.com>
2024-01-08 14:55:13 +0200

    wrap the Main component to make dropable and added style

--- todo-list/src/components/Main.tsx
+++ todo-list/src/components/Main.tsx
@@ -2,18 +2,36 @@ import { FC, useContext, useState } from 'react';
 import { Tasking } from '../context/TaskProvider';
 import SingleTodo from './SingleTodo';
 import InputFields from './InputFields';
+import { Droppable } from 'react-beautiful-dnd';
 
 const Main: FC = () => {
   const [todo, setTodo] = useState<string>('');
   const { state } = useContext(Tasking);
 
   return (
-    <div className="py-5  flex-1 bg-white">
+    <main className="py-5  flex-1 bg-green-600">
       <InputFields todo={todo} setTodo={setTodo} />
-      {state.tasks.map(({ todo, id, isDone }) => (
-        <SingleTodo todo={todo} id={id} isDone={isDone} />
-      ))}
-    </div>
+      <Droppable droppableId="TodosList">
+        {(provided) => (
+          <section
+            className="bg-red-400 mx-auto w-[1000px] p-5"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {state.tasks.map(({ todo, id, isDone }, index) => (
+              <SingleTodo
+                todo={todo}
+                id={id}
+                isDone={isDone}
+                key={id}
+                index={index}
+              />
+            ))}
+            {provided.placeholder}
+          </section>
+        )}
+      </Droppable>
+    </main>
   );
 };
 
