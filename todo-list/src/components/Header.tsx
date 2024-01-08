55f165d341ad5ee2c9a9eafd33cbe2e6b7521ae1
gashaw mucheye <gashaw2533@gmail.com>
2024-01-08 14:53:23 +0200

    styled the Header component

--- todo-list/src/components/Header.tsx
+++ todo-list/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 const Header: FC = () => {
   return (
-    <div className="bg-[#2c6bb8] text-center p-5">
-      <h1 className="text-4xl font-bold mb-3">Todo List</h1>
-    </div>
+    <header className="bg-[#2c6bb8] text-center p-5">
+      <h1 className="bg-red-400 mx-auto w-[1000px] p-5 text-4xl font-bold">
+        Todo List
+      </h1>
+    </header>
   );
 };
 
