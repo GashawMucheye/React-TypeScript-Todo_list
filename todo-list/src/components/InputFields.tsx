d0dd69b511324d8bfdac51f6acdd75bc233a64b0
gashaw mucheye <gashaw2533@gmail.com>
2024-01-08 14:54:18 +0200

    added style to inputFields component

--- todo-list/src/components/InputFields.tsx
+++ todo-list/src/components/InputFields.tsx
@@ -15,14 +15,21 @@ const InputFields: FC<Props> = ({ todo, setTodo }) => {
     });
   };
   return (
-    <div>
+    <div className="bg-blue-950 mx-auto w-[1000px] p-5">
       <input
+        className="w-[90%] text-center rounded-lg"
         type="text"
+        placeholder="Enter Your Task Here"
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
           setTodo(e.target.value)
         }
       />
-      <button onClick={handleAdd}>Add</button>
+      <button
+        className="text-center bg-blue-600 px-[12px] rounded-full"
+        onClick={handleAdd}
+      >
+        Add
+      </button>
     </div>
   );
 };
