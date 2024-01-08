bac9268ee66059ec13a60002cd6151c7bc26e5b4
gashaw mucheye <gashaw2533@gmail.com>
2024-01-08 14:51:56 +0200

    styled the footer by tailwind css

--- todo-list/src/components/Footer.tsx
+++ todo-list/src/components/Footer.tsx
@@ -3,7 +3,9 @@ import { FC } from 'react';
 const Footer: FC = () => {
   return (
     <footer className="bg-[#2c6bb8] text-center p-5">
-      <p className="w-3/4 mx-auto border-none"> &copy; All Rights Reserved</p>
+      <p className="bg-red-400 mx-auto w-[1000px] p-5">
+        &copy; All Rights Reserved
+      </p>
     </footer>
   );
 };
