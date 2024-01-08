fb39d11de73950c8a27f3ed9fa6aff3af5c83ead
gashaw mucheye <gashaw2533@gmail.com>
2024-01-08 14:49:33 +0200

    import  DragDropContext to and create function onDragEnd to make drag and drop

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,17 +1,37 @@
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import { useContext } from 'react';
+import { Tasking } from './context/TaskProvider';
 
 const App = () => {
+  const { dispatch } = useContext(Tasking);
+
+  const onDragEnd = (result: DropResult) => {
+    console.log(result);
+
+    if (!result.destination) {
+      return; // Drop outside the list
+    }
+
+    dispatch({
+      type: 'REORDER_TASK',
+      sourceIndex: result.source.index,
+      destinationIndex: result.destination.index,
+    });
+  };
   return (
-    <div
-      className="h-screen text-center flex bg-[#b2a7a7] 
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div
+        className="h-screen text-center flex bg-[#b2a7a7] 
   flex-col"
-    >
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+      >
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </DragDropContext>
   );
 };
 
